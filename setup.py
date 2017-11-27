49a6b4655b08592d3faf8b13a0704999eaf4e23c
Pavlo Vozniuk <antipsih@ukr.net>
2017-11-27 12:53:41 +0000

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+from short_numbers import __version__, __author__
+
+setup(
+    name='short_numbers',
+    version=__version__,
+    author=__author__,
+    url='https://github.com/pvoznyuk/short-numbers',
+    author_email='pavlo.vozniuk@gmail.com',
+    scripts=['short_numbers']
+)
